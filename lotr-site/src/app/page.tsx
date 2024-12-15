26fce245b6f38ded8e9bf38f2323e0396391a8e0
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-14 19:57:55 -0800

    Update page.tsx

--- lotr-site/src/app/page.tsx
+++ lotr-site/src/app/page.tsx
@@ -1,17 +1,17 @@
 "use client"
 
 import axios from "axios";
-import { use, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { Book } from "./types/types"
 
 export default function Home() {
 
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(null)
+  // const [loading, setLoading] = useState(false)
+  // const [error, setError] = useState(null)
   const [book, setBook] = useState<Book[]>([])
 
   useEffect(() => {
-    setLoading(true)
+    // setLoading(true)
 
     // Fetch from local API route (not external API)
     axios
@@ -19,17 +19,17 @@ export default function Home() {
       .then((response) => {
         setBook(response.data.docs)
       })
-      .catch((error) => {
-        setError(error)
+      .catch(() => {
+        // setError(error)
       })
       .finally(() => {
-        setLoading(false)
+        // setLoading(false)
       })
   }, [])
 
   // console.log(book.map(b => b.name))
   const books = book.map(b => {
-    return <p>{b.name}</p>
+    return <p key={b.id}>{b.name}</p>
   })
   return (
     <main>

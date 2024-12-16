f86c13282e4d269a8ad2ed63443c881e8a7d8492
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-15 20:10:44 -0800

    Adjusted text size in credits page

--- lotr-site/src/app/credits/page.tsx
+++ lotr-site/src/app/credits/page.tsx
@@ -6,14 +6,14 @@ export default function Credits() {
         <section className='flex flex-col justify-center items-center gap-y-10 m-4 md:m-12'>
             <div className='flex flex-col justify-center items-center gap-y-10 bg-white
                 p-6 md:p-12 rounded-3xl'>
-                <h2 className='text-3xl lg:text-5xl xl:text-7xl'>Credits</h2>
+                <h2 className='text-xl lg:text-5xl xl:text-4xl'>Credits</h2>
                 <div className='flex flex-col justify-center items-center gap-y-10 text-center
-                text-lg lg:text-3xl xl:text-4xl px-10'>
+                text-xl lg:text-3xl xl:text-3xl px-10'>
                     <p className=''>This project uses data provided by the oneAPI licensed under
                         Creative Commons Attribution 4.0 International
                     </p>
                     <Link
-                        className='underline text-lg lg:text-3xl xl:text-4xl'
+                        className='underline text-xl lg:text-3xl xl:text-3xl'
                         href="https://the-one-api.dev/"
                         target="_blank"
                         rel="noopener noreferrer"
@@ -21,7 +21,7 @@ export default function Credits() {
                         Original source
                     </Link>
                     <Link
-                        className='underline text-lg lg:text-3xl xl:text-4xl'
+                        className='underline text-xl lg:text-3xl xl:text-3xl'
                         href="https://creativecommons.org/licenses/by/4.0/"
                         target="_blank"
                         rel="noopener noreferrer"

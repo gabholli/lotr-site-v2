399a41eaaea58c5893213538e3b970fdca26967d
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-12-17 19:10:06 -0800

    Widened button for mobile screens

--- lotr-site/src/app/characters/page.tsx
+++ lotr-site/src/app/characters/page.tsx
@@ -105,7 +105,7 @@ export default function CharactersList() {
                     />
                     <button
                         type="submit"
-                        className="px-4 py-2 bg-blue-500 text-white rounded">
+                        className="px-4 py-2 bg-blue-500 text-white rounded w-full">
                         Search
                     </button>
                 </form>
